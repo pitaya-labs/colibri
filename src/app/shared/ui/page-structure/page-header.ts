6e2d95369809100f00af8bad4c2df80a1d1eb868
Beto Ramirez <maramirez10@gmail.com>
2026-09-02 10:34:22 -0600

    added create-edit header and page-header-information

--- src/app/shared/ui/page-structure/page-header.ts
+++ src/app/shared/ui/page-structure/page-header.ts
@@ -12,12 +12,21 @@ import { Component } from '@angular/core';
         font-size: 26px;
         font-weight: 400;
       }
+
+      .info {
+        font-size: 14px;
+        color: var(--color-gray-400);
+        margin: 6px 0 0 0;
+      }
     }
   `,
   template: `
     <header class="page-header">
       <div class="title">
         <ng-content select="[pageHeaderTitle]"></ng-content>
+        <div class="info">
+          <ng-content select="[pageHeaderInformation]"> </ng-content>
+        </div>
       </div>
       <ng-content select="[pageHeaderActions]"></ng-content>
     </header>
